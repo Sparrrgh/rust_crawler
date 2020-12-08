3a18e9c2ed09ac99c089bacf3391897750ad16a7
Sparrrgh <ma.bellia.98@gmail.com>
2020-12-08 17:29:14 +0100

    Visit and screenshot at the same time
    Instead of screenshotting the endpoints after visiting all of them I'll just screenshot them if they're answering.
    This should have some improvement in performance on large-scale scans.

--- src/main.rs
+++ src/main.rs
@@ -4,74 +4,67 @@ use chrono::{Local};
 use std::io::{self, BufRead};
 use failure;
 use std::{process,env};
-use std::sync::Arc;
-use regex::Regex;
-use hyper::{Client, StatusCode};
-use tokio::{fs,task};
 use futures::future::join_all;
+use std::sync::{Arc,Mutex};
+use regex::Regex;
+use hyper::{Client};
+use tokio::{fs,task};
 use hyper_tls::HttpsConnector;
 use hyper_timeout::TimeoutConnector;
 use std::time::Duration;
 
 fn screenshot_site(tab: Arc<Tab>, site: String, path: String) -> Result<(),failure::Error>{
-    let re = Regex::new(r"^(https|http)://")?;
+    let re = Regex::new(r"^(https?)://")?;
     let png_data = tab.set_default_timeout(Duration::from_secs(5))
-        .navigate_to(&site)?
-        .capture_screenshot(ScreenshotFormat::PNG, None, true)?;
+    .navigate_to(&site)?
+    .capture_screenshot(ScreenshotFormat::PNG, None, true)?;
+    //Replace http(s), to not break the filename
     let finpath = format!("{}/{}{}.png", path, re.replace(&site, ""),Local::now());
-    println!("{}", finpath);    
+    println!("{}", finpath);
+    //TODO: Use tokio fs
     let _ = write(finpath, png_data)?;
     Ok(())
 }
 
-async fn visit_site(site: String) -> Result<String,failure::Error>{
-    let https = HttpsConnector::new();
-    let mut connector = TimeoutConnector::new(https);
+async fn visit_site(site: String, browser: Arc<Mutex<Browser>>, path: String) -> Result<(),failure::Error>{
+    //Set timeouts for hyper-timeout
+    let mut connector = TimeoutConnector::new(HttpsConnector::new());
     connector.set_connect_timeout(Some(Duration::from_secs(5)));
-    connector.set_read_timeout(Some(Duration::from_secs(5)));
-    connector.set_write_timeout(Some(Duration::from_secs(5)));
+    connector.set_read_timeout(Some(Duration::from_secs(10)));
+    connector.set_write_timeout(Some(Duration::from_secs(10)));
     let client = Client::builder().build::<_, hyper::Body>(connector);
     let uri = site.parse()?;
-    //TODO: Tell user when an endpoint had a timeout
     let _resp = match client.get(uri).await{
-        Ok(x) => x,
-        Err(..) => return Ok(String::from(""))
+        Ok(..) => {
+            println!("Visiting {}", &site);
+            //Unlock the browser's mutex, create a new tab and use it to screenshot the page
+            let tab = browser.lock().unwrap().new_tab().unwrap();
+            let _ = task::spawn_blocking(|| {
+                let _ = screenshot_site(tab,site,path);
+            }).await;
+        },
+        //This code sucks and probably the perf is horrible, but I couldn't find how to match io::ErrorKind::TimedOut
+        //TODO: Find a way to use `io::ErrorKind::TimedOut`
+        Err(e)=> if e.into_cause().unwrap().to_string() == "deadline has elapsed" {println!("{} timed out", &site)}
     };
-    println!("Visited site {}", site);
-    Ok(site)
+    Ok(())
 }
 
 #[tokio::main]
 async fn screenshot_block(endpoints: Vec<String>, path: String) -> Result<(),failure::Error>{
-    let mut futures = vec![];
     let browser = Browser::new(LaunchOptionsBuilder::default().build().unwrap())?;
+    let arc_browser = Arc::new(Mutex::new(browser));
+    let mut fut = vec![];
+    //Create directory where the screenshots will be saved
     if create_dir(format!("{}",path)).is_err(){println!("Directory \"{}\" already exists", path); process::exit(-1);}
     for e in endpoints{
-        let fut = task::spawn(visit_site(e));
-        futures.push(fut);
-        //This does NOT scale
-        //It will create a huge vector full of empty strings!
-        //Maybe use Option instead of Result, I guess pushing none renders an empty array anyways
-    }
-    let results = join_all(futures).await;
-    //Remove empty strings
-    let res_e = results.iter()
-        .map(|x| x.as_ref().unwrap().as_ref().unwrap())
-        .filter(|x| **x != String::from(""))
-        .map(|x| x.to_string());
-    
-    //Here I screenshot each endpoint that responded
-    //Can crash if it encounters some weird responding endpoints
-    //Ex: Basic auth
-    //TODO: Catch crash
-    println!("Screenshotting responding endpoints");
-    for r in res_e{
-        let new_tab = browser.new_tab();
+        let carc_browser = arc_browser.clone();
         let cpath = path.clone();
-        let _ = task::spawn_blocking(|| {
-            let _ = screenshot_site(new_tab.unwrap(),r,cpath);
-        }).await;
+        //Spawn a task for each site to visit, push the future returned in the vector to join later
+        let f = task::spawn(visit_site(e, carc_browser, cpath));
+        fut.push(f);
     }
+    let _ = join_all(fut).await;
     Ok(())
 }
 
